11ef3bd60491ea3d5e58a7aab08a1f9f7c9f883b
Knut Sveidqvist <knut@sveido.com>
2016-08-27 17:14:46 +0200

    Adding runner logic to avoid config file

---
+++ runner.js
@@ -0,0 +1,46 @@
+/**
+ * Created by knut on 2016-04-17.
+ */
+var crib = require('./src/index.js');
+
+module.exports.start = function(conf) {
+    var fs = require('fs');
+    var path = require('path');
+
+    var cribHome = process.env.CRIB_HOME;
+    if (!cribHome) {
+        cribHome = process.cwd();
+    }
+
+    console.log('Started crib using ', cribHome);
+
+    var conf = {
+        plugins: []
+    };
+
+    var modulesPath = path.join(cribHome, 'crib_modules');
+
+    var files = fs.readdirSync(modulesPath);
+
+    if (files.indexOf('crib-mq') < 0) {
+        console.log('Error, crib-mq not found aborting');
+        process.exit(0);
+    }
+
+    if (files.indexOf('crib-storage') < 0) {
+        console.log('Error, crib-storage not found aborting');
+        process.exit(0);
+    }
+
+    conf.plugins.push('crib-mq');
+    conf.plugins.push('crib-storage');
+
+    files.forEach((file)=> {
+        if (file !== 'crib-mq' && file !== 'crib-storage' && file.match('crib-')) {
+            console.log('Using plugin: ', file);
+            conf.plugins.push(file);
+        }
+    });
+
+    crib.start(conf);
+}
