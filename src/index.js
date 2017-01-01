152b4860a1104422ca8e562d5c8579b1c1f21a7e
Knut Sveidqvist <knut@sveido.com>
2017-01-01 18:19:56 +0100

    Timezone update

--- src/index.js
+++ src/index.js
@@ -3,50 +3,49 @@
  */
 var pm2 = require('pm2');
 
+module.exports.start = function(conf) {
+  console.log('cwd ', process.cwd());
 
+  pm2.connect(function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(2);
+    }
 
-module.exports.start = function(conf){
-    console.log('cwd ',process.cwd());
-
-    pm2.connect(function(err) {
-        if (err) {
-            console.error(err);
-            process.exit(2);
+    // conf.plugins.push('crib-mq');
+    conf.plugins.forEach(function(plugin) {
+      console.log('Starting ', conf.modulesPath + '/' + plugin + '/src/index.js');
+      pm2.start({
+        script: conf.modulesPath + '/' + plugin + '/src/index.js', // Script to be run
+        exec_mode: 'cluster', // Allow your app to be clustered
+        instances: 1, // Optional: Scale your app by 4
+        name: plugin,
+        max_memory_restart: '100M', // Optional: Restart your app if it reaches 100Mo
+        log_file: 'logs/' + plugin + '.log',
+        env: {
+          'CRIB_BUSS_URL': process.env.CRIB_BUSS_URL,
+          'CRIB_LOGGLY_TOKEN': process.env.CRIB_LOGGLY_TOKEN,
+          'CRIB_LOGGLY_DOMAIN': process.env.CRIB_LOGGLY_DOMAIN,
+          'TZ': process.env.TZ
         }
-
-        // conf.plugins.push('crib-mq');
-        conf.plugins.forEach(function (plugin) {
-            console.log('Starting ',conf.modulesPath + '/'+ plugin + '/src/index.js');
-            pm2.start({
-                script: conf.modulesPath + '/' + plugin + '/src/index.js',         // Script to be run
-                exec_mode: 'cluster',        // Allow your app to be clustered
-                instances: 1,                // Optional: Scale your app by 4
-                name: plugin,
-                max_memory_restart: '100M',   // Optional: Restart your app if it reaches 100Mo
-                log_file: "logs/"+plugin+".log",
-                env: {
-                    "CRIB_BUSS_URL": process.env.CRIB_BUSS_URL,
-                    "CRIB_LOGGLY_TOKEN": process.env.CRIB_LOGGLY_TOKEN,
-                    "CRIB_LOGGLY_DOMAIN": process.env.CRIB_LOGGLY_DOMAIN
-                }
-            }, function (err, apps) {
-                console.log(err);
-                pm2.disconnect();
-            });
-            console.log('Started ',plugin);
-        });
-
-        // console.log('Starting crib-ui');
-        // pm2.start({
-        //     script: '../crib-ui/src/index.js',         // Script to be run
-        //     exec_mode: 'cluster',        // Allow your app to be clustered
-        //     instances: 1,                // Optional: Scale your app by 4
-        //     name: 'crib-ui',
-        //     max_memory_restart: '100M',   // Optional: Restart your app if it reaches 100Mo
-        //     log_file: "logs/crib-ui.log",
-        // }, function (err, apps) {
-        //     console.log(err);
-        //     pm2.disconnect();
-        // });
+      }, function(err) {
+        console.log(err);
+        pm2.disconnect();
+      });
+      console.log('Started ', plugin);
     });
+
+    // console.log('Starting crib-ui');
+    // pm2.start({
+    //     script: '../crib-ui/src/index.js',         // Script to be run
+    //     exec_mode: 'cluster',        // Allow your app to be clustered
+    //     instances: 1,                // Optional: Scale your app by 4
+    //     name: 'crib-ui',
+    //     max_memory_restart: '100M',   // Optional: Restart your app if it reaches 100Mo
+    //     log_file: 'logs/crib-ui.log",
+    // }, function (err, apps) {
+    //     console.log(err);
+    //     pm2.disconnect();
+    // });
+  });
 };
--- src/runner.js
+++ src/runner.js
@@ -4,45 +4,45 @@
 var crib = require('./index.js');
 
 module.exports.start = function(conf) {
-    var fs = require('fs');
-    var path = require('path');
+  var fs = require('fs');
+  var path = require('path');
 
-    var cribHome = process.env.CRIB_HOME;
-    if (!cribHome) {
-        cribHome = process.cwd();
+  var cribHome = process.env.CRIB_HOME;
+  if (!cribHome) {
+    cribHome = process.cwd();
+  }
+
+  console.log('Started crib using ', cribHome);
+
+  var conf = {
+    plugins: []
+  };
+
+  var modulesPath = path.join(cribHome, 'crib_modules');
+
+  conf.modulesPath = modulesPath;
+
+  var files = fs.readdirSync(modulesPath);
+
+  if (files.indexOf('crib-mq') < 0) {
+    console.log('Error, crib-mq not found aborting');
+    process.exit(0);
+  }
+
+  if (files.indexOf('crib-storage') < 0) {
+    console.log('Error, crib-storage not found aborting');
+    process.exit(0);
+  }
+
+  conf.plugins.push('crib-mq');
+  conf.plugins.push('crib-storage');
+
+  files.forEach((file) => {
+    if (file !== 'crib-mq' && file !== 'crib-storage' && file !== 'crib-ui' && file.match('crib-')) {
+      console.log('Using plugin: ', file);
+      conf.plugins.push(file);
     }
+  });
 
-    console.log('Started crib using ', cribHome);
-
-    var conf = {
-        plugins: []
-    };
-
-    var modulesPath = path.join(cribHome, 'crib_modules');
-
-    conf.modulesPath = modulesPath;
-
-    var files = fs.readdirSync(modulesPath);
-
-    if (files.indexOf('crib-mq') < 0) {
-        console.log('Error, crib-mq not found aborting');
-        process.exit(0);
-    }
-
-    if (files.indexOf('crib-storage') < 0) {
-        console.log('Error, crib-storage not found aborting');
-        process.exit(0);
-    }
-
-    conf.plugins.push('crib-mq');
-    conf.plugins.push('crib-storage');
-
-    files.forEach((file)=> {
-        if (file !== 'crib-mq' && file !== 'crib-storage' && file !== 'crib-ui' && file.match('crib-')) {
-            console.log('Using plugin: ', file);
-            conf.plugins.push(file);
-        }
-    });
-
-    crib.start(conf);
+  crib.start(conf);
 }
